cf6672059bd8297160b236a84880e964e9055359
Murchas <80643884+Murchas@users.noreply.github.com>
2021-03-14 20:27:40 +0100

    Add files via upload

---
+++ deposito/src/main/java/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+        public void operativa_cuenta(float cantidad) {
+             CCuenta Cuenta1;
+        double saldoActual;
+      
+        Cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = Cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            Cuenta1.retirar(2300);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            Cuenta1.ingresar(695);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }       
+        }
+}
